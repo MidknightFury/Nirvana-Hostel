825cb33fc9846cd11c453e086b4343c71783e37f
mohit <mohit@gmail.com>
2021-07-31 21:56:55 +0530

    Added Dialog

--- app/src/main/java/com/example/project/activity_studentrequest.java
+++ app/src/main/java/com/example/project/activity_studentrequest.java
@@ -1,8 +1,12 @@
 package com.example.project;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class activity_studentrequest extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class activity_studentrequest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studentrequest);
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(activity_studentrequest.this);
+                ViewGroup viewGroup = findViewById(android.R.id.content);
+                View dialogView = LayoutInflater.from(v.getContext()).inflate(R.layout.customview, viewGroup, false);
+                builder.setView(dialogView);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
     }
 }
