a0a7d20ffab984241084d690885cdc8e0fbc57a3
MidknightFury <manjitsharma20k1@gmail.com>
2021-08-02 12:47:02 +0530

    about to be final commit 2

--- app/src/main/java/com/example/project/CustomExpandableListAdapter.java
+++ app/src/main/java/com/example/project/CustomExpandableListAdapter.java
@@ -13,7 +13,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
+//import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 
 public class CustomExpandableListAdapter extends BaseExpandableListAdapter {
